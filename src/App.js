4a7e03604489f5a9b7acf8be9bb2b06247a648bb
Keirrison cauet <keirrisoncauet31@gmail.com>
2025-11-21 12:55:14 -0300

    Header já feito, falta só o efeito do hover

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <section>
+          <h2>Café - Aurora</h2>
+        </section>
+        <section style={{
+          display: 'flex',
+          gap: '45px',
+          fontSize: '18px'
+        }}>
+          <p>Página principal</p>
+          <p>Contato</p>
+          <p>Galeria</p>
+        </section>
+      </header>
+
+
       
     </div>
   );
